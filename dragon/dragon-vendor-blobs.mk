UBERROMS/vendor_google

dragon: update proprietary blobs for 7.1.2 (N2G47D)

dragon/dragon-vendor-blobs.mk
@@ -15,6 +15,7 @@
 PRODUCT_COPY_FILES += \
     vendor/google_devices/dragon/proprietary/bin/tsec_service:system/bin/tsec_service \
     vendor/google_devices/dragon/proprietary/bin/vkinfo:system/bin/vkinfo \
+    vendor/google_devices/dragon/proprietary/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml \
     vendor/google_devices/dragon/proprietary/acr_ucode.bin:root/vendor/firmware/nouveau/acr_ucode.bin \
     vendor/google_devices/dragon/proprietary/fecs.bin:root/vendor/firmware/nouveau/fecs.bin \
     vendor/google_devices/dragon/proprietary/fecs_sig.bin:root/vendor/firmware/nouveau/fecs_sig.bin \

Myst3rion09/proprietary_vendor_google

Change-Id: I51d6f9701bc34ae60c92626d05a904784ae6c1b4

dragon/dragon-vendor-blobs.mk
@@ -15,6 +15,7 @@
 PRODUCT_COPY_FILES += \
     vendor/google/dragon/proprietary/bin/tsec_service:system/bin/tsec_service \
     vendor/google/dragon/proprietary/bin/vkinfo:system/bin/vkinfo \
+    vendor/google/dragon/proprietary/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml \
     vendor/google/dragon/proprietary/acr_ucode.bin:root/vendor/firmware/nouveau/acr_ucode.bin \
     vendor/google/dragon/proprietary/fecs.bin:root/vendor/firmware/nouveau/fecs.bin \
     vendor/google/dragon/proprietary/fecs_sig.bin:root/vendor/firmware/nouveau/fecs_sig.bin \